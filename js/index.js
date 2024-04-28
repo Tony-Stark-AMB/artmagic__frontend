fb31973f59cb4b809aa00eea40dc7b4cf995a04b
Tony-Stark-AMB <tony.stark.amb@gmail.com>
2024-04-28 23:49:52 +0300

    end basket modal and add product page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-// for admin panel bug fixing
-if(screen.width > 1200)
-document.querySelector('.swiper-menu').style.height = `${document.querySelector('.catalog-menu').clientHeight}px`;
-else 
-document.querySelector('.swiper-menu').style.height = "";
+const cardInput = document.querySelector(".card-input");
+const walletInput = document.querySelector(".wallet-input");
 
+const cardSvg = document.querySelector('.card-img');
+const walletSvg = document.querySelector('.wallet-img');
+
+const cardNumberInput = document.getElementById("card-input")
+
+cardSvg.style.color = '#3e77aa'; // Change color for card SVG
+walletSvg.style.color = '#7d7d7d'; // Reset color for wallet SVG
+
+function handleInputChange() {
+    if (cardInput.checked) {
+        cardNumberInput.style.display = "block";
+        cardSvg.style.color = '#3e77aa'; // Change color for card SVG
+        walletSvg.style.color = '#7d7d7d'; // Reset color for wallet SVG
+    } else if (walletInput.checked) {
+        cardNumberInput.style.display = "none";
+        walletSvg.style.color = '#3e77aa'; // Change color for wallet SVG
+        cardSvg.style.color = '7d7d7d'; // Reset color for card SVG
+    }
+}
+
+cardInput.addEventListener('change', handleInputChange);
+walletInput.addEventListener('change', handleInputChange);
